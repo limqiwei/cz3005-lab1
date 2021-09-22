b24fe74a833f3331352b1131a01763b57977dd66
Lim Qi Wei <qiwei.cse@gmail.com>
2021-09-23 02:02:41 +0800

    Added code using networkx library for shortest path

--- Task 3/task3-tests.py
+++ Task 3/task3-tests.py
@@ -1,10 +1,10 @@
-import task3
+import subgraph_gen
 
 
 # Insert test functions here
 
 def testGetDistance():
-    assert task3.getStraightLineDistance(0,0,3,4) == 5, "Should be 5"
+    assert subgraph_gen.getStraightLineDistance([0,0],[3,4]) == 5, "Should be 5"
 
 # Insert test functions here under here to run when this python script is run.
 if __name__ == "__main__":
