d7b5273db0fcd6b32c5fcf8d34256bee47d340ab
wing lam <winglam209@gmail.com>
2021-09-25 23:07:08 +0800

    updated UCS, to remove parts used for testing before submission

--- Task 2/rand_graph.py
+++ Task 2/rand_graph.py
@@ -2,58 +2,99 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import json
-# from networkx.readwrite import json_graph
+import networkx.algorithms.isomorphism as iso
 
 
 nodeNum = int(input("Number of nodes: "))
 
-ug = nx.fast_gnp_random_graph(nodeNum, 0.9, seed=None, directed=False)
+G = nx.fast_gnp_random_graph(nodeNum, 0.9, seed=None, directed=False)
 
-# print("Nodes of graph: ")
-# print(ug.nodes())
-# print("Edges of graph: ")
-# print(ug.edges())
-# print("")
-print("Number of edges: " + str(len(ug.edges)))
+print(f"Number of edges: {len(G.edges)}")
 
-G = {}
-Cost = {}
-f1 = open("G_test.json", "w")
-f2 = open("Cost_test.json", "w")
+G_dict = {}
+Cost_dict = {}
+G_test = open("G_test.json", "w")
+Cost_test = open("Cost_test.json", "w")
 
-for node in ug.nodes():
+# for node in G.nodes():
+for (u,v) in G.edges():
+    G.edges[u,v]['weight'] = random.randint(1,20)
+    weight = G.edges[u,v]['weight']
+    G.edges[v,u]['weight'] = weight
+    edge1 = str(u) + "," + str(v)
+    edge2 = str(v) + "," + str(u)
+    Cost_dict[edge1] = weight
+    Cost_dict[edge2] = weight
+    
+    # print(f"{u,v}, {G.edges[u,v]['weight']}")
+    # print(f"{v,u}, {G.edges[v,u]['weight']}")
+
+for node in G.nodes():
     e = []
-    for (u,v) in ug.edges():
-        ug.edges[u,v]['weight'] = random.randint(1,20)
-        weight = ug.edges[u,v]['weight']
-        ug.edges[v, u]['weight'] = weight
-
+    for (u,v) in G.edges():
         if node == u:
             e.append(str(v))
-            G[str(u)] = e
-            edge1 = str(u) + "," + str(v)
-            edge2 = str(v) + "," + str(u)
-            Cost[edge1] = weight
-            Cost[edge2] = weight
+            G_dict[str(u)] = e
         if node == v:
             e.append(str(u))
-            G[str(v)] = e
+            G_dict[str(v)] = e
 
-# print(G)
-# print(Cost)
+G_json = json.dumps(G_dict)
+G_test.write(G_json)
+Cost_json = json.dumps(Cost_dict)
+Cost_test.write(Cost_json)
 
-G_json = json.dumps(G)
-f1.write(G_json)
-Cost_json = json.dumps(Cost)
-f2.write(Cost_json)
+G_test.close()
+Cost_test.close()
 
-f1.close()
-f2.close()
-
-labels = {e: ug.edges[e]['weight'] for e in ug.edges}
-pos = nx.circular_layout(ug)
-
-nx.draw(ug, with_labels=True)
-nx.draw_networkx_edge_labels(ug, pos, edge_labels=labels)
+# weight_labels = nx.get_edge_attributes(G, 'weight')    
+# print(weight_labels)
+pos = nx.circular_layout(G)
+nx.draw(G, with_labels=True)
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=weight_labels)
 
 plt.show()
+
+'''
+GRAPH = json.load(open("G.json", "r"))
+COST = json.load(open("Cost.json", "r"))
+DIST = json.load(open("Dist.json", "r"))
+
+# GRAPH = json.load(open("G_test.json", "r"))
+# COST = json.load(open("Cost_test.json", "r"))
+
+TARGET = "50"
+SOURCE = "1"
+ENERGY_CONSTRAINT = 287932
+
+G1 = nx.Graph(GRAPH)
+# print(f"Isomorphic? {nx.is_isomorphic(G, G1)}")
+
+for (u,v) in G1.edges():
+    edge1 = str(u) + "," + str(v)
+    edge2 = str(v) + "," + str(u)
+
+    # G1.edges[u,v]['weight'] = COST[edge1]
+    # G1.edges[v,u]['weight'] = COST[edge2]
+    G1.edges[u,v]['weight'] = DIST[edge1]
+    G1.edges[v,u]['weight'] = DIST[edge2]
+
+    # print(f"edge1 {edge1}")
+    # print(f"COST {COST[edge1]}")
+    # print(f"edge2 {edge2}")
+    # print(f"COST {COST[edge2]}")
+
+shortest_path = nx.dijkstra_path(G1, source="1", target="50", weight='weight')
+print(shortest_path)
+
+# em = iso.numerical_edge_match("weight", 1)
+# print(f"Isomorphic? {nx.is_isomorphic(G, G1, edge_match=em)}")
+
+# weight_labels1 = {(u,v): G1.edges[u,v]['weight'] for (u,v) in G1.edges}
+
+# pos1 = nx.circular_layout(G1)
+# nx.draw(G1, with_labels=True)
+# nx.draw_networkx_edge_labels(G1, pos1, edge_labels=weight_labels1)
+
+# plt.show()
+'''
--- Task 2/task2.py
+++ Task 2/task2.py
@@ -3,7 +3,6 @@ from queue import PriorityQueue
 import time
 
 
-
 def neighbours(node, graph):
     return graph[node]
 
@@ -34,7 +33,7 @@ def UCS(SOURCE, TARGET, GRAPH, COST, DIST, ENERGY):
 
         if current == TARGET:
             break
-
+ 
         for next in neighbours(current, GRAPH):
             next_cost = get_cost(current, next, COST)
             new_cost = cost_so_far[current] + next_cost
@@ -43,11 +42,11 @@ def UCS(SOURCE, TARGET, GRAPH, COST, DIST, ENERGY):
             new_dist = dist_so_far[current] + distance_to_next
 
             if next not in visited or new_dist < dist_so_far[next]:
-                if new_cost <= ENERGY:
-                    dist_so_far[next] = new_dist
-                    cost_so_far[next] = new_cost
-                    queue.put((new_dist, next))
-                    visited[next] = current
+                # if new_cost <= ENERGY:
+                dist_so_far[next] = new_dist
+                cost_so_far[next] = new_cost
+                queue.put((new_dist, next))
+                visited[next] = current
 
     return visited
 
@@ -71,11 +70,26 @@ def get_path(SOURCE, TARGET, visited, COST, DIST):
     for node in range(len(path)-1):
         path_str += str(path[node]) + " -> "
     
-    # path_str += TARGET
     path_str += str(path[node+1])
+
+    global path_list 
+    path_list = list(map(str, path))
 
     return path_str, total_cost, total_dist
 
+
+# remove function before submission
+def search():
+    GRAPH = json.load(open("G.json", "r"))
+    COST = json.load(open("Cost.json", "r"))
+    DIST = json.load(open("Dist.json", "r"))
+    TARGET = "50"
+    SOURCE = "1"
+    ENERGY_CONSTRAINT = 287932
+
+    visited = UCS(SOURCE, TARGET, GRAPH, COST, DIST, ENERGY_CONSTRAINT)
+    get_path(SOURCE, TARGET, visited, COST, DIST)
+    
 
 def main():
     print("running...")
@@ -88,10 +102,10 @@ def main():
     SOURCE = "1"
     ENERGY_CONSTRAINT = 287932
 
-    # graph = json.load(open("G_test.json", "r"))
-    # cost = json.load(open("Cost_test.json", "r"))
-    # target = "3"
-    # source = "0"
+    # # graph = json.load(open("G_test.json", "r"))
+    # # cost = json.load(open("Cost_test.json", "r"))
+    # # target = "3"
+    # # source = "0"
 
     print("searching...")
     start_time = time.time()
@@ -102,16 +116,20 @@ def main():
     end_time = time.time()
 
     print("")
-    print("Shortest path: " + str(path))
+    print(f"Shortest path: {path}")
     print("")
-    print("Shortest distance: " + str(total_dist))
-    print("Total energy cost: " + str(total_cost))
+    print(f"Shortest distance: {total_dist}")
+    print(f"Total energy cost: {total_cost}")
     print("")
 
-    time_taken = str(end_time - start_time)
-    print("time taken: "+ time_taken + " sec")
-    print("terminating...")
+    time_taken = end_time - start_time
+    print(f"Time taken: {time_taken} sec \n")
 
 
 if __name__ == "__main__":
     main()
+
+# remove before submission
+else:
+    search()
+
